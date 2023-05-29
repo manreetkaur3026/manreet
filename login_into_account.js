38608d0bef86ef95c17fa58c2d6487ee24b8736b
manreetkaur3026 <132334001+manreetkaur3026@users.noreply.github.com>
2023-05-29 14:37:14 +0530

    login_into_account.js

--- login_into_account.js
+++ login_into_account.js
@@ -1,1 +1,10 @@
 
+async function createMultipleTestRuns(filenames) {
+  try {
+    const response = await axios.get(`${testrailApiUrl}/get_cases/1`, {
+      auth: {
+        username: 'csecec.niharikaverma@gmail.com',
+        password: 'Bebo@1234'
+      }
+    });
+  
